71920f147a21e037bd8d65a57e4165af72264cc0
Yuhui <julie.bao32@gmail.com>
2020-04-12 14:24:29 -0400

    update pages.c and implement bitmap.c

--- pages.c
+++ pages.c
@@ -1,3 +1,4 @@
+// based on cs3650 starter code
 
 #define _GNU_SOURCE
 #include <string.h>
@@ -14,6 +15,7 @@
 
 #include "pages.h"
 #include "util.h"
+#include "bitmap.h"
 
 const int PAGE_COUNT = 256;
 const int NUFS_SIZE  = 4096 * 256; // 1MB
@@ -22,26 +24,19 @@ static int   pages_fd   = -1;
 static void* pages_base =  0;
 
 void
-pages_init(const char* path, int create)
+pages_init(const char* path)
 {
-    if (create) {
-        pages_fd = open(path, O_CREAT | O_EXCL | O_RDWR, 0644);
-        assert(pages_fd != -1);
+    pages_fd = open(path, O_CREAT | O_RDWR, 0644);
+    assert(pages_fd != -1);
 
-        int rv = ftruncate(pages_fd, NUFS_SIZE);
-        assert(rv == 0);
-    }
-    else {
-        pages_fd = open(path, O_RDWR);
-        assert(pages_fd != -1);
-    }
+    int rv = ftruncate(pages_fd, NUFS_SIZE);
+    assert(rv == 0);
 
     pages_base = mmap(0, NUFS_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, pages_fd, 0);
     assert(pages_base != MAP_FAILED);
 
-    // mark inode 0 as taken
-    char* pbase = (char*) pages_base;
-    pbase[0] = 1;
+    void* pbm = get_pages_bitmap();
+    bitmap_put(pbm, 0, 1);
 }
 
 void
@@ -55,5 +50,42 @@ void*
 pages_get_page(int pnum)
 {
     return pages_base + 4096 * pnum;
+}
+
+void*
+get_pages_bitmap()
+{
+    return pages_get_page(0);
+}
+
+void*
+get_inode_bitmap()
+{
+    uint8_t* page = pages_get_page(0);
+    return (void*)(page + 32);
+}
+
+int
+alloc_page()
+{
+    void* pbm = get_pages_bitmap();
+
+    for (int ii = 1; ii < PAGE_COUNT; ++ii) {
+        if (!bitmap_get(pbm, ii)) {
+            bitmap_put(pbm, ii, 1);
+            printf("+ alloc_page() -> %d\n", ii);
+            return ii;
+        }
+    }
+
+    return -1;
+}
+
+void
+free_page(int pnum)
+{
+    printf("+ free_page(%d)\n", pnum);
+    void* pbm = get_pages_bitmap();
+    bitmap_put(pbm, pnum, 0);
 }
 
